872426cf4378195a11b7fad889143aae57fd6501
Lucas Holanda Cardoso <lucas.cardoso2@bancobmg.com.br>
2026-06-17 16:58:18 -0300

    feat: add violacao - OCP 👽

---
+++ src/Solid/DevWiki.Solid.Ocp/Violacao/CalculadoraTarifa.cs
@@ -0,0 +1,12 @@
+﻿namespace DevWiki.Solid.Ocp.Violacao;
+
+public class CalculadoraTarifa
+{
+    public decimal Calcular(string tipoPagamento, decimal valor) => tipoPagamento switch
+    {
+        "PIX" => valor * 0.98m,
+        "TED" => valor * 0.97m,
+        "Boleto" => valor * 0.95m,
+        _ => throw new NotImplementedException()
+    };
+}
---
+++ src/Solid/DevWiki.Solid.Ocp/Violacao/GeradorRelatorio.cs
@@ -0,0 +1,32 @@
+﻿using DevWiki.Solid.Shared.Domain;
+
+namespace DevWiki.Solid.Ocp.Violacao;
+
+public class GeradorRelatorio
+{
+    public string Gerar(RelatorioCobranca relatorio, string formato) => formato switch
+    {
+        "TXT" => GerarTxt(relatorio),
+        "CSV" => GerarCsv(relatorio),
+        _ => throw new ArgumentException($"formato {formato} n suportado meu nobre")
+    };
+
+    private static string GerarTxt(RelatorioCobranca relatorio)
+    {
+        var linhas = relatorio.Cobrancas.Select(c => $"  {c.Id} | R$ {c.Valor:F2} | {c.Descricao} | Vence: {c.DataVencimento}");
+
+        return $"""
+                relatorio - periodo: {relatorio.Periodo}
+                total processado: R$ {relatorio.TotalProcessado:F2}
+
+                {string.Join(Environment.NewLine, linhas)}
+            """;
+    }
+
+    private static string GerarCsv(RelatorioCobranca relatorio)
+    {
+        var linhas = relatorio.Cobrancas.Select(c => $"{c.Id};{c.Valor};{c.Descricao};{c.DataVencimento}");
+
+        return $"Id,Valor,Descricao,DataVencimento\n{string.Join('\n', linhas)}";
+    }
+}
