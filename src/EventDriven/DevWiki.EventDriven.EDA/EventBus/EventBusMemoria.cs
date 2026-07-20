d91ce716f9cff508a7ce315d2e1807fcd37a777e
Lucas Holanda Cardoso <lucas.cardoso2@bancobmg.com.br>
2026-07-20 13:30:59 -0300

    feat: implementacao em memoria do EventBus

---
+++ src/EventDriven/DevWiki.EventDriven.EDA/EventBus/EventBusMemoria.cs
@@ -0,0 +1,15 @@
+﻿using DevWiki.EventDriven.EDA.Interfaces;
+using DevWiki.EventDriven.Shared.Events;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevWiki.EventDriven.EDA.EventBus;
+
+public sealed class EventBusMemoria(IServiceProvider serviceProvider) : IEventBus
+{
+    public async Task PublicarAsync<TEvento>(TEvento evento, CancellationToken ct = default) where TEvento : DomainEvent
+    {
+        var handlers = serviceProvider.GetServices<IEventHandler<TEvento>>();
+        var tarefas = handlers.Select(h => h.HandleAsync(evento, ct));
+        await Task.WhenAll(tarefas);
+    }
+}
