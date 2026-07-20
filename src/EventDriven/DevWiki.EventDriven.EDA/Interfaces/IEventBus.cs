73581568d5d7d13c316a35f53f4c755faa24b051
Lucas Holanda Cardoso <lucas.cardoso2@bancobmg.com.br>
2026-07-20 13:18:07 -0300

    feat: add contratos: bus e handlers de eventos padrao EDA PubSub em memoria

---
+++ src/EventDriven/DevWiki.EventDriven.EDA/Interfaces/IEventBus.cs
@@ -0,0 +1,9 @@
+﻿using DevWiki.EventDriven.Shared.Events;
+
+namespace DevWiki.EventDriven.EDA.Interfaces;
+
+public interface IEventBus
+{
+    Task PublicarAsync<TEvento>(TEvento evento, CancellationToken ct = default)
+        where TEvento : DomainEvent;
+}
---
+++ src/EventDriven/DevWiki.EventDriven.EDA/Interfaces/IEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using DevWiki.EventDriven.Shared.Events;
+
+namespace DevWiki.EventDriven.EDA.Interfaces;
+
+public interface IEventHandler<TEvento> where TEvento : DomainEvent
+{
+    Task HandleAsync(TEvento evento, CancellationToken ct = default);
+}
