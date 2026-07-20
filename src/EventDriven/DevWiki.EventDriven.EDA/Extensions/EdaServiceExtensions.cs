e5914841805c64219a99cfd9b6eae3fc06f480bd
Lucas Holanda Cardoso <lucas.cardoso2@bancobmg.com.br>
2026-07-20 13:37:15 -0300

    feat: add extensao DI p registrar o EventBus e tds os handlers do modulo EDA

---
+++ src/EventDriven/DevWiki.EventDriven.EDA/Extensions/EdaServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using DevWiki.EventDriven.EDA.EventBus;
+using DevWiki.EventDriven.EDA.Events;
+using DevWiki.EventDriven.EDA.Handlers;
+using DevWiki.EventDriven.EDA.Interfaces;
+using DevWiki.EventDriven.EDA.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevWiki.EventDriven.EDA.Extensions;
+
+public static class EdaServiceExtensions
+{
+    public static void AddEdaServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IEventBus, EventBusMemoria>();
+        services.AddSingleton<ServicoProcessamentoPagamento>();
+
+        services.AddSingleton<IEventHandler<PagamentoProcessadoEvento>, EnviarNotificacaoPagamentoHandler>();
+        services.AddSingleton<IEventHandler<PagamentoProcessadoEvento>, AtualizarExtratoPagamentoHandler>();
+        services.AddSingleton<IEventHandler<PagamentoProcessadoEvento>, ComputarPontosPagamentoHandler>();
+        services.AddSingleton<IEventHandler<FraudeDetectadaEvento>, AlertarFraudeHandler>();
+    }
+}
